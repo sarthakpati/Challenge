6a46a64c0e6a1d1841a83ebad7465446ab53190c
sarthakpati <sarthak.pati@hotmail.com>
2021-09-10 15:20:24 -0400

    cli finished

--- Task_1/fets_challenge/generate_predictions.py
+++ Task_1/fets_challenge/generate_predictions.py
@@ -3,18 +3,30 @@ import os, argparse
 from fets_challenge import model_outputs_to_disc
 
 
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="FeTS_Challenge_Inference",
         formatter_class=argparse.RawTextHelpFormatter,
-        description="Generate and save predictions from trained models.\n\n"
+        description="Generate and save predictions from trained models.\n\n",
     )
 
-    parser.add_argument( "-d", "--data_dir", type=str, required=True, help="Path to the directory containing the data on which inference is to be run." )
-    parser.add_argument( "-m", "--models_dir", type=str, required=True, help="Path to the directory containing all the trained models." )
-    
+    parser.add_argument(
+        "-d",
+        "--data_dir",
+        type=str,
+        required=True,
+        help="Path to the directory containing the data on which inference is to be run.",
+    )
+    parser.add_argument(
+        "-m",
+        "--models_dir",
+        type=str,
+        required=True,
+        help="Path to the directory containing all the trained models.",
+    )
+    parser.add_argument(
+        "-0", "--out_dir", type=str, required=True, help="Path to the output directory."
+    )
     parser.add_argument(
         "-device",
         default="cuda",
